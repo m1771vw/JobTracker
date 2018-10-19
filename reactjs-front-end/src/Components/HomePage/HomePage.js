5b5f9f1cacc799a4645034376ded2ec57a72d54f
William Yang <williamyang93@gmail.com>
2018-10-19 01:02:48 -0700

    Working on homepage

--- reactjs-front-end/src/Components/HomePage/HomePage.js
+++ reactjs-front-end/src/Components/HomePage/HomePage.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-
+import Header from '../Navbar';
 
 class HomePage extends Component {
 
     
     render() {
         return (
-            <div class="hero-image">
-          <div class="hero-text">
-            <h1>I am John Doe</h1>
-            <p>And I'm a Photographer</p>
-            <button>Hire me</button>
-          </div>
-        </div>
+            <div>
+            <Header title="Home Page"/>
+            <div className="hero-image">
+            <div className="hero-text">
+                <h1>I am John Doe</h1>
+                <p>And I'm a Photographer</p>
+                <button>Hire me</button>
+            </div>
+            </div>
+            </div>
+
      
         );
     }
