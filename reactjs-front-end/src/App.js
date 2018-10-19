6f576ee84e75677494e8c26adf4e3ada608957c2
Doogie Nguyen <dsnguye1@gmail.com>
2018-10-18 17:05:58 -0700

    Working on Add Job

--- reactjs-front-end/src/App.js
+++ reactjs-front-end/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Dashboard from'./Components/Dashboard/Dashboard';
+import AddJob from'./Components/Dashboard/AddJob';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-    <Dashboard/>
+    <AddJob/>
     );
   }
 }
